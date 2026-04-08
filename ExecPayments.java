d953cf9cf364a579f9a0de6ea735559f4b05d86e
Michael Misi <dmichael.misi@gmail.com>
2026-04-09 00:09:20 +0200

    Showing Grand total and average total of the day

--- ExecPayments.java
+++ ExecPayments.java
@@ -1,14 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.*;
 import accounting.Payment;
 import accounting.VATRate;
 
 public class ExecPayments {
     public static void main(String[] args) {
-        Payment myPayment = new ExpressBankTransfer();
 
-        double netAmount = myPayment.perform(10.00); // Montant après frais bancaires
-        double totalAmount = VATRate.STANDARD.calculateTotalAmount(netAmount);
+        List<Payment> transactionBatch = new ArrayList<>();
 
-        System.out.println("Net Amount (Excl. VAT): " + netAmount);
-        System.out.println("Final Amount (Incl. VAT): " + totalAmount);
+        transactionBatch.add(new CreditCard());
+        transactionBatch.add(new BankTransfer());
+        transactionBatch.add(new ExpressBankTransfer());
+        transactionBatch.add(new CreditCard());
+
+        double baseAmount = 100.0;
+        double totalAmount = 0;
+
+        System.out.println("=== Daily Transaction Report ===");
+        System.out.println("Number of transactions: " + transactionBatch.size());
+        System.out.println("--------------------------------");
+
+        double streamTotal = transactionBatch.stream()
+        // .filter(p -> p instanceof CreditCard)
+        // For every payment, we calculate the base amount
+        .mapToDouble(p -> VATRate.STANDARD.calculateTotalAmount(p.perform(baseAmount)))
+        // We sum up everything
+        .sum();
+
+        double streamAverage = transactionBatch.stream()
+        .mapToDouble(p -> VATRate.STANDARD.calculateTotalAmount(p.perform(baseAmount)))
+        .average()
+        .orElse(0.0);
+
+        transactionBatch.stream().forEach(p -> {
+            double total = VATRate.STANDARD.calculateTotalAmount(p.perform(baseAmount));
+            
+            System.out.println("Method: " + p.getClass().getSimpleName());
+            System.out.println("Final amount to pay: " + total + " €");
+            System.out.println("---");
+        });
+
+        System.out.println("Grand Total for the day : " + streamTotal + " €.");
+        System.out.println("Average for the day : " + streamAverage + " €.");
     }
 }
